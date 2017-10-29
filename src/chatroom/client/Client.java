23a5ab89c187f441368686fc24e1e01819c208b3
AlexKue <a.kuehnle@hotmail.de>
2017-10-29 14:45:42 +0100

    * Started working on a listening Thread

--- src/chatroom/client/Client.java
+++ src/chatroom/client/Client.java
@@ -1,36 +1,40 @@
 package chatroom.client;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import chatroom.model.Message;
+import chatroom.serializer.MessageSerializer;
+
+import java.io.*;
+
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        MessageSerializer s = new MessageSerializer();
         try {
             System.out.println("Please enter the adress of the server to connect to: ");
             String address = sc.nextLine();
 
             Socket server = new Socket(sc.nextLine(),54322);
             System.out.println("Connected to server!");
-            
-            InputStream in = server.getInputStream();
-            OutputStream out = server.getOutputStream();
-            DataInputStream dataIn = new DataInputStream(new BufferedInputStream(in));
-            DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(out));
-            
-            while(true){
-                dataOut.writeUTF(sc.nextLine());
-                dataOut.flush();
-                System.out.println(dataIn.readUTF());
-            }
-            
+
+            ObjectInputStream dataIn = new ObjectInputStream(new BufferedInputStream(server.getInputStream()));
+            ObjectOutputStream dataOut = new ObjectOutputStream(new BufferedOutputStream(server.getOutputStream()));
+
+            Thread clientListener = new Thread(() -> {
+                try {
+                    Message m = (Message)dataIn.readObject();
+                    //TODO: Deserialize incoming message
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            });
+
+
+
         } catch (IOException ex) {
             System.err.println("Connection Error!");
             ex.printStackTrace();
