d6b40cf311c0926166f5d0483bb4dc3f32724976
AlexKue <A.kuehnle@hotmail.de>
2017-10-30 00:04:25 +0100

    * made class abstract

--- src/chatroom/serializer/MessageSerializer.java
+++ src/chatroom/serializer/MessageSerializer.java
@@ -1,17 +1,11 @@
 package chatroom.serializer;
 
 import chatroom.model.Message;
-
-public class MessageSerializer<T> {
     /*
     * The MessageSerializer is a class to handle different types of messages.
     * While for a simple chatprogram this might seem overengineered, it helps expanding the possibilities for
     * different types of messages to be send like files, whispering functionality or images if whished.
-    *
     * */
-    public Message serialize(){
-        return null;
-    }
-    //TODO: Deserializer
 
+public abstract class MessageSerializer {
 }
