dc974848cdc8ef3e83080839cc8a6bc791087480
AlexKue <A.kuehnle@hotmail.de>
2017-10-31 23:56:00 +0100

    * added Hashmap of serializers

--- src/chatroom/server/listener/UserListeningThread.java
+++ src/chatroom/server/listener/UserListeningThread.java
@@ -1,32 +1,45 @@
 package chatroom.server.listener;
 
 import chatroom.model.Message;
+import chatroom.model.ServerMessage;
+import chatroom.serializer.MessageSerializer;
+import chatroom.serializer.PublicTextMessageSerializer;
+import chatroom.serializer.ServerMessageSerializer;
+import chatroom.serializer.TargetedTextMessageSerializer;
 import chatroom.server.listener.MessageListener;
 import chatroom.model.User;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class UserListeningThread extends Thread {
     private User user;
     private MessageListener listener;
+    public HashMap<Byte, MessageSerializer> serializerHashMap;
 
     public UserListeningThread(User user){
         this.user = user;
+        serializerHashMap = new HashMap<>();
+        serializerHashMap.put((byte) 0,new ServerMessageSerializer());
+        serializerHashMap.put((byte) 1, new PublicTextMessageSerializer());
+        serializerHashMap.put((byte) 2, new TargetedTextMessageSerializer());
     }
 
     public void run(){
         try {
-            listener.getMessageQueue().put((Message)user.getDataIn().readObject());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            Message m = deserialize(user.getDataIn().readByte());
+            listener.getMessageQueue().put(m);
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
     }
+    public Message deserialize(Byte type){
+        //TODO deserialize implementation
+        return null;
+    }
+
 
     public User getUser() {
         return user;
