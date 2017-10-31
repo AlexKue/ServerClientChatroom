5fb49acbfe40d7376324dd190fcf8d88f55619f9
AlexKue <A.kuehnle@hotmail.de>
2017-10-31 23:54:45 +0100

    * did stuff

--- src/chatroom/serializer/ServerMessageSerializer.java
+++ src/chatroom/serializer/ServerMessageSerializer.java
@@ -1,4 +1,14 @@
 package chatroom.serializer;
 
+import chatroom.model.Message;
+import chatroom.model.ServerMessage;
+
 public class ServerMessageSerializer extends MessageSerializer{
+
+    public void serialize(Message m){
+
+    }
+    public ServerMessage deserialize(String m){
+        return new ServerMessage(m);
+    }
 }
