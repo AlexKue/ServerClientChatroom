a0b0f06900f7bcaba5938eb1a17bb948399b1425
AlexKue <A.kuehnle@hotmail.de>
2017-10-31 23:55:04 +0100

    * removed stuff

--- src/chatroom/serializer/UserMessageSerializer.java
+++ src/chatroom/serializer/UserMessageSerializer.java
@@ -3,5 +3,4 @@ package chatroom.serializer;
 import chatroom.model.Message;
 
 public abstract class UserMessageSerializer extends MessageSerializer{
-    abstract public String deserialize(Message m);
 }
