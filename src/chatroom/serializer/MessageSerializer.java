dfb2aacae4c7c7ab2d5cbc34534cd770eafb9f7e
ri93xuc <ri93xuc@ppc809.mirz.uni-jena.de>
2017-11-03 14:04:04 +0100

    * added Javadocs

--- src/chatroom/serializer/MessageSerializer.java
+++ src/chatroom/serializer/MessageSerializer.java
@@ -5,13 +5,30 @@ import chatroom.model.Message;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-    /*
-    * The MessageSerializer is a class to handle different types of messages.
-    * While for a simple chatprogram this might seem overengineered, it helps expanding the possibilities for
-    * different types of messages to be send like files, whispering functionality or images if whished.
-    * */
+/**
+ * Handles serialization so that the listening/sending threads of the server
+ * and client can send and receive those through the stream. Instead of using
+ * <code>Serializable</code> interface, an own implementation is used.
+ */
 
 public abstract class MessageSerializer {
+    /**
+     * Prepares an Message object so that it can be sent through an 
+     * <code>OutputStream</code>.
+     * @param out an <code>OutputStream</code> to send the message through 
+     */
     abstract public void serialize(OutputStream out);
+    
+    /**
+     * Deserializes an incoming message of a client.
+     * When the user of the client sends a message, a <code>Byte</code> is sent
+     * to clarify which type of message is sent. Afterwards, multiple Objects 
+     * may be sent, which this method receives and put them into an object 
+     * inheriting <code>Message</code>
+     *  
+     * @param in the <code>InputStream</code> the message will received from
+     * @return an Object inheriting the abstract class <code>Message</code>
+     * @throws IOException 
+     */
     abstract public Message deserialize(InputStream in) throws IOException;
 }
