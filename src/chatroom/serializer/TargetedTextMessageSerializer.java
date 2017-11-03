f2d36078d0e88ceddb453a0dc619358b2e6d1ce2
ri93xuc <ri93xuc@ppc809.mirz.uni-jena.de>
2017-11-03 15:31:43 +0100

    * fixed stuff (??)

--- src/chatroom/serializer/TargetedTextMessageSerializer.java
+++ src/chatroom/serializer/TargetedTextMessageSerializer.java
@@ -10,29 +10,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class TargetedTextMessageSerializer extends UserMessageSerializer{
+public class TargetedTextMessageSerializer extends UserMessageSerializer {
 
+    @Override
+    public Message deserialize(InputStream in) throws IOException {
+        String message = ((DataInputStream) in).readUTF();
+        String sender = ((DataInputStream) in).readUTF();
+        String receiver = ((DataInputStream) in).readUTF();
+        return new TargetedTextMessage(message, sender, receiver);
+    }
 
     @Override
     public void serialize(OutputStream out, Message m) {
         try {
             DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(out));
-            dataOut.writeByte((byte)2);
-            dataOut.writeUTF(((TargetedTextMessage)m).getMessage());
-            dataOut.writeUTF(((TargetedTextMessage)m).getSender());
-            dataOut.writeUTF(((TargetedTextMessage)m).getReceiver());
+            dataOut.writeByte((byte) 2);
+            dataOut.writeUTF(((TargetedTextMessage) m).getMessage());
+            dataOut.writeUTF(((TargetedTextMessage) m).getSender());
+            dataOut.writeUTF(((TargetedTextMessage) m).getReceiver());
             dataOut.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        
-    }
-
-    @Override
-    public Message deserialize(InputStream in) throws IOException {
-        String message = ((DataInputStream)in).readUTF();
-        String sender = ((DataInputStream)in).readUTF();
-        String receiver = ((DataInputStream)in).readUTF();
-        return new TargetedTextMessage(message,sender,receiver);
     }
 }
