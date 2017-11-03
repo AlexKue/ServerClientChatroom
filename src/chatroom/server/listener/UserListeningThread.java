341a6add7880de773a0003801fee9386acf9b190
ri93xuc <ri93xuc@ppc809.mirz.uni-jena.de>
2017-11-03 15:23:23 +0100

    * added serializer

--- src/chatroom/server/listener/UserListeningThread.java
+++ src/chatroom/server/listener/UserListeningThread.java
@@ -1,11 +1,8 @@
 package chatroom.server.listener;
 
 import chatroom.model.Message;
-import chatroom.serializer.MessageSerializer;
-import chatroom.serializer.PublicTextMessageSerializer;
-import chatroom.serializer.ServerMessageSerializer;
-import chatroom.serializer.TargetedTextMessageSerializer;
 import chatroom.model.User;
+import chatroom.serializer.Serializer;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 
@@ -16,25 +13,22 @@ public class UserListeningThread extends Thread {
 
     private final User user;
     private MessageListener listener;
-    public HashMap<Byte, MessageSerializer> serializerHashMap;
     private final DataInputStream userDataIn;
+    private final Serializer serializer;
 
     public UserListeningThread(User user) {
         this.user = user;
         userDataIn = new DataInputStream(new BufferedInputStream(user.getIn()));
-        serializerHashMap = new HashMap<>();
-        serializerHashMap.put((byte) 0, new ServerMessageSerializer());
-        serializerHashMap.put((byte) 1, new PublicTextMessageSerializer());
-        serializerHashMap.put((byte) 2, new TargetedTextMessageSerializer());
+        serializer = new Serializer();
+
     }
 
     @Override
     public void run() {
         while (true) {
-            try {
-                //Get the serializer depending on incoming byte
-                MessageSerializer serializer = serializerHashMap.get(userDataIn.readByte());
-                Message m = serializer..deserialize(userDataIn);
+            try {               
+                byte type = userDataIn.readByte();
+                Message m = serializer.deserialize(userDataIn, type);
                 listener.getMessageQueue().put(m);
             } catch (IOException e) {
                 e.printStackTrace();
