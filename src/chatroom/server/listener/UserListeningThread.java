03611e6c4dba6e681cc6d1e790f5b2a807ace2a0
ri93xuc <ri93xuc@ppc809.mirz.uni-jena.de>
2017-11-03 13:53:02 +0100

    * added Javadocs

--- src/chatroom/server/listener/UserListeningThread.java
+++ src/chatroom/server/listener/UserListeningThread.java
@@ -1,45 +1,48 @@
 package chatroom.server.listener;
 
 import chatroom.model.Message;
-import chatroom.model.ServerMessage;
 import chatroom.serializer.MessageSerializer;
 import chatroom.serializer.PublicTextMessageSerializer;
 import chatroom.serializer.ServerMessageSerializer;
 import chatroom.serializer.TargetedTextMessageSerializer;
-import chatroom.server.listener.MessageListener;
 import chatroom.model.User;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 
 import java.io.IOException;
 import java.util.HashMap;
 
 public class UserListeningThread extends Thread {
-    private User user;
+
+    private final User user;
     private MessageListener listener;
     public HashMap<Byte, MessageSerializer> serializerHashMap;
+    private final DataInputStream userDataIn;
 
-    public UserListeningThread(User user){
+    public UserListeningThread(User user) {
         this.user = user;
+        userDataIn = new DataInputStream(new BufferedInputStream(user.getIn()));
         serializerHashMap = new HashMap<>();
-        serializerHashMap.put((byte) 0,new ServerMessageSerializer());
+        serializerHashMap.put((byte) 0, new ServerMessageSerializer());
         serializerHashMap.put((byte) 1, new PublicTextMessageSerializer());
         serializerHashMap.put((byte) 2, new TargetedTextMessageSerializer());
     }
 
-    public void run(){
-        try {
-            Message m = deserialize(user.getDataIn().readByte());
-            listener.getMessageQueue().put(m);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                //Get the serializer depending on incoming byte
+                MessageSerializer serializer = serializerHashMap.get(userDataIn.readByte());
+                Message m = serializer..deserialize(userDataIn);
+                listener.getMessageQueue().put(m);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-    public Message deserialize(Byte type){
-        //TODO deserialize implementation
-        return null;
-    }
-
 
     public User getUser() {
         return user;
