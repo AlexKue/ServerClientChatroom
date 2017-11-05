cf36883d46a5417a241062ebb5c77063fa83ec84
AlexKue <A.kuehnle@hotmail.de>
2017-11-05 18:57:34 +0100

    * moved out of main, implemented methods

--- src/chatroom/server/Server.java
+++ src/chatroom/server/Server.java
@@ -2,25 +2,82 @@ package chatroom.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Scanner;
+
+import chatroom.model.MessageTypeDictionary;
 import chatroom.server.listener.MessageListener;
 import chatroom.server.listener.NetworkListener;
 
 public class Server {
-    public static void main (String args[]){
+    private ServerSocket listener;
+    private NetworkListener networkListener;
+    private MessageListener messageListener;
+    private MessageTypeDictionary messageTypeDictionary;
+    private Scanner sc;
+    private boolean isRunning;
 
+    public Server(){
+        messageTypeDictionary = new MessageTypeDictionary();
+        sc = new Scanner(System.in);
+        isRunning = true;
+    }
+
+    public void start(){
         try {
-            ServerSocket listener = new ServerSocket(54322);
-            NetworkListener networkListener = new NetworkListener(listener);
-            MessageListener messageListener = new MessageListener(networkListener);
+            listener = new ServerSocket(54322);
+            networkListener = new NetworkListener(this);
+            messageListener = new MessageListener(this);
             System.out.println("Server Online!");
-            networkListener.start();
-            messageListener.start();
-            
+            getNetworkListener().start();
+            getMessageListener().start();
+
         } catch (IOException ex) {
             System.err.println("Error while starting the server!");
             ex.printStackTrace();
         }
-        
-    } 
-    
+
+        while(isRunning()){
+            if(sc.nextLine().trim().equals("!quit")){
+                stop(listener);
+            }
+        }
+    }
+    private void stop(ServerSocket listener){
+        System.out.println("*** Shutting down Server... ***");
+        isRunning = false;
+        try {
+            System.out.println("- Closing the Server Socket");
+            listener.close();
+            getMessageListener().interrupt();
+        } catch (IOException e) {
+            System.err.println("*** Error while closing the server! ***");
+            e.printStackTrace();
+        }
+    }
+    public boolean isRunning(){
+        return isRunning;
+    }
+
+    public static void main (String args[]){
+        Server server = new Server();
+        server.start();
+    }
+
+    public ServerSocket getListener() {
+        return listener;
+    }
+
+
+    public NetworkListener getNetworkListener() {
+        return networkListener;
+    }
+
+
+    public MessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public MessageTypeDictionary getMessageTypeDictionary() {
+        return messageTypeDictionary;
+    }
 }
