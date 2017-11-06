73adcc9b20f3f07458e126dadd1b21e93bbb7827
alexkue <alexander.kuehnle@uni-jena.de>
2017-11-06 13:05:22 +0100

    * TODO: reverse lookup

--- src/chatroom/model/MessageTypeDictionary.java
+++ src/chatroom/model/MessageTypeDictionary.java
@@ -4,23 +4,24 @@ import java.util.HashMap;
 
 public class MessageTypeDictionary {
 
-   public enum MessageType{
-      SERVERMSG, PUBLICTEXTMSG, TARGETTEXTMSG, LOGINMSG, LOGOUTMSG
-   }
-   public MessageType getType(byte type){
-      return messageTypeHashMap.get(type);
-   }
+    public enum MessageType {
+        SERVERMSG, PUBLICTEXTMSG, TARGETTEXTMSG, LOGINMSG, LOGOUTMSG
+    }
 
-   private HashMap<Byte, MessageType> messageTypeHashMap;
+    public MessageType getType(byte type) {
+        return messageTypeHashMap.get(type);
+    }
+    //TODO: Reverse lookup Hashmap?
+    private HashMap<Byte, MessageType> messageTypeHashMap;
 
-   public MessageTypeDictionary(){
-       messageTypeHashMap = new HashMap<>();
-       messageTypeHashMap.put((byte)0,MessageType.SERVERMSG);
-       messageTypeHashMap.put((byte)1,MessageType.PUBLICTEXTMSG);
-       messageTypeHashMap.put((byte)2,MessageType.TARGETTEXTMSG);
-       messageTypeHashMap.put((byte)3,MessageType.LOGINMSG);
-       messageTypeHashMap.put((byte)4,MessageType.LOGOUTMSG);
-   }
+    public MessageTypeDictionary() {
+        messageTypeHashMap = new HashMap<>();
+        messageTypeHashMap.put((byte) 0, MessageType.SERVERMSG);
+        messageTypeHashMap.put((byte) 1, MessageType.PUBLICTEXTMSG);
+        messageTypeHashMap.put((byte) 2, MessageType.TARGETTEXTMSG);
+        messageTypeHashMap.put((byte) 3, MessageType.LOGINMSG);
+        messageTypeHashMap.put((byte) 4, MessageType.LOGOUTMSG);
+    }
 
 
 }
