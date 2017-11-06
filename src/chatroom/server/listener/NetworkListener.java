0d7ebe2f0a862904972970f81d7ebe033165862e
AlexKue <A.kuehnle@hotmail.de>
2017-11-06 02:28:38 +0100

    * shutdown procedure

--- src/chatroom/server/listener/NetworkListener.java
+++ src/chatroom/server/listener/NetworkListener.java
@@ -5,35 +5,53 @@ import java.net.ServerSocket;
 import java.util.ArrayList;
 
 import chatroom.model.User;
+import chatroom.server.Server;
 
 public class NetworkListener extends Thread {
 
-    private ServerSocket listener;
+    private Server server;
     private ArrayList<UserListeningThread> userListeningThreadList;
 
-    public NetworkListener(ServerSocket listener) {
+    public NetworkListener(Server server) {
         this.userListeningThreadList = new ArrayList<>();
-        this.listener = listener;
+        this.server = server;
     }
 
     @Override
     public void run() {
-        while (true) {
+        while (server.isRunning()) {
             try {
+                User user = new User(server.getListener().accept());
+                //TODO: Thread pools (see executer)
                 //Create new Thread for the MessageListener for each new client
-                UserListeningThread u = new UserListeningThread(new User(listener.accept()));
+                UserListeningThread u = new UserListeningThread(user, server);
                 getUserListeningThreadList().add(u);
                 u.start();
-
-
             } catch (IOException ex) {
-                System.err.println("Connection between server and new client failed!");
-                ex.printStackTrace();
+                System.out.println();
+                //.accept() will throw if server is closing
             }
+
         }
+        shutdown();
     }
 
     public ArrayList<UserListeningThread> getUserListeningThreadList() {
         return userListeningThreadList;
+    }
+
+    public void shutdown(){
+        System.out.println("*** Shutting down network listener ***");
+        System.out.println("- Closing sockets of Client in List");
+        for(UserListeningThread u : userListeningThreadList){
+            try {
+                u.getUser().getIn().close();
+                u.getUser().getOut().close();
+                u.getUser().getSocket().close();
+            } catch (IOException e) {
+                //No need to handle since we close the server anyways
+            }
+
+        }
     }
 }
