3043821939fd9f59b451ca3b3af2123951bacff7
AlexKue <A.kuehnle@hotmail.de>
2017-11-06 02:25:46 +0100

    * added message displaying

--- src/chatroom/client/ClientListeningThread.java
+++ src/chatroom/client/ClientListeningThread.java
@@ -1,7 +1,8 @@
 package chatroom.client;
 
-import chatroom.model.Message;
+import chatroom.model.*;
 import chatroom.serializer.Serializer;
+
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 
@@ -9,26 +10,50 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ClientListeningThread extends Thread {
-    InputStream in;
-    Serializer serializer;
+    private Client client;
+    private InputStream in;
+    private Serializer serializer;
 
-    public ClientListeningThread(InputStream in) throws IOException, ClassNotFoundException {
+    public ClientListeningThread(InputStream in, Client client) throws IOException {
         this.in = in;
         serializer = new Serializer();
+        this.client = client;
     }
 
-    public void run(){
-        DataInputStream dataIn = new DataInputStream(new BufferedInputStream(in));
-        while(true){
+    @Override
+    public void run() {
+        boolean isRunning = true;
+        while (isRunning) {
             try {
-                byte type = dataIn.readByte();
+                byte type = (byte)in.read();
                 Message m = serializer.deserialize(in, type);
-                //TODO: Display text
-                
+                display(m, type);
+
             } catch (IOException ex) {
+                System.err.println("Connection Lost!");
                 ex.printStackTrace();
+                isRunning = false;
             }
         }
+    }
+
+    private void display(Message message, byte type) {
+        switch (client.getMessageTypeDictionary().getType(type)) {
+            case PUBLICTEXTMSG:
+                PublicTextMessage publicTextMessage = ((PublicTextMessage) message);
+                String publicString = publicTextMessage.getSender() + ": " + publicTextMessage.getMessage();
+                System.out.println(publicString);
+                break;
+            case SERVERMSG:
+                ServerMessage serverMessage = ((ServerMessage) message);
+                System.out.println("*** " + serverMessage.getMessage() + " ***");
+                break;
+            case TARGETTEXTMSG:
+                TargetedTextMessage targetedTextMessage = ((TargetedTextMessage) message);
+                String targetedString = targetedTextMessage.getSender() + " (whispered): " + targetedTextMessage.getMessage();
+                System.out.println(targetedString);
+        }
+
     }
 
 }
