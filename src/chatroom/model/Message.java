b26847278443fe624a50580ac6ce9b6b8d9115f6
AlexKue <A.kuehnle@hotmail.de>
2017-11-07 20:02:56 +0100

    * added transient id

--- src/chatroom/model/Message.java
+++ src/chatroom/model/Message.java
@@ -2,8 +2,17 @@ package chatroom.model;
 
 public abstract class Message {
     protected byte type;
+    private transient int id;
 
     public byte getType(){
         return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
