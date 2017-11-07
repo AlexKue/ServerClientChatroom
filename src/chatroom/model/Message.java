f7577900a619361bd5d16b5cfa672d7ec06603c3
BlackFox1197 <ckwdani@gmail.com>
2017-11-07 15:50:05 +0100

    * removed unnecessary stuff

--- src/chatroom/model/Message.java
+++ src/chatroom/model/Message.java
@@ -3,13 +3,6 @@ package chatroom.model;
 public abstract class Message {
     protected byte type;
 
-    /**
-     * Returns the type of Message, defined by a byte. 
-     * Possible values: 0: ServerMessage
-     *                  1: PublicTextMessage
-     *                  2: TargetedTextMessage
-     * @return 
-     */
     public byte getType(){
         return type;
     }
