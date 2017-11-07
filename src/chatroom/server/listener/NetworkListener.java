772bba084801df7c0d814b93cc3fe5d8b5533e95
BlackFox1197 <ckwdani@gmail.com>
2017-11-07 15:52:06 +0100

    * added reference to user storage

--- src/chatroom/server/listener/NetworkListener.java
+++ src/chatroom/server/listener/NetworkListener.java
@@ -2,39 +2,48 @@ package chatroom.server.listener;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import chatroom.model.User;
+import chatroom.model.UserStorage;
 import chatroom.server.Server;
 
 public class NetworkListener extends Thread {
 
     private Server server;
     private ArrayList<UserListeningThread> userListeningThreadList;
+    private UserStorage userStorage;
+    private int idCounter;
 
     public NetworkListener(Server server) {
         this.userListeningThreadList = new ArrayList<>();
         this.server = server;
+        this.userStorage = new UserStorage();
+        idCounter = 0;
     }
 
     @Override
     public void run() {
         while (server.isRunning()) {
             try {
-                User user = new User(server.getListener().accept());
+                User user = new User(server.getListener().accept(), idCounter);
+                ++idCounter;
                 //TODO: Thread pools (see executer)
                 //Create new Thread for the MessageListener for each new client
                 UserListeningThread u = new UserListeningThread(user, server);
                 getUserListeningThreadList().add(u);
                 u.start();
             } catch (IOException ex) {
-                System.out.println("*** Connection Error!" + ex.toString() + ") ***");
+                System.out.println("*** Connection Error! " + ex.toString() + ") ***");
                 //.accept() will throw if server is closing
             }
 
         }
         shutdown();
     }
+
     //TODO: Proper Logout of Client
 //    public void removeClient(){
 //
