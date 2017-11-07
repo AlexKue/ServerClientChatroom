54caa97d428284377e831c2778bd54f567c0016c
AlexKue <A.kuehnle@hotmail.de>
2017-11-08 00:23:35 +0100

    * added new msgtyes to hashmap

--- src/chatroom/serializer/Serializer.java
+++ src/chatroom/serializer/Serializer.java
@@ -1,6 +1,8 @@
 package chatroom.serializer;
 
 import chatroom.model.Message;
+import chatroom.model.MessageTypeDictionary;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -9,12 +11,16 @@ import java.util.HashMap;
 public class Serializer {
 
     private final HashMap<Byte, MessageSerializer> serializerHashMap;
+    private final MessageTypeDictionary dict;
 
     public Serializer() {
+        dict = new MessageTypeDictionary();
         serializerHashMap = new HashMap<>();
-        serializerHashMap.put((byte) 0, new ServerMessageSerializer());
-        serializerHashMap.put((byte) 1, new PublicTextMessageSerializer());
-        serializerHashMap.put((byte) 2, new TargetedTextMessageSerializer());
+        serializerHashMap.put(dict.getByte(MessageTypeDictionary.MessageType.PUBLICSERVERMSG), new PublicServerMessageSerializer());
+        serializerHashMap.put(dict.getByte(MessageTypeDictionary.MessageType.PUBLICTEXTMSG), new PublicTextMessageSerializer());
+        serializerHashMap.put(dict.getByte(MessageTypeDictionary.MessageType.TARGETTEXTMSG), new TargetedTextMessageSerializer());
+        serializerHashMap.put(dict.getByte(MessageTypeDictionary.MessageType.LOGINMSG), new LoginMessageSerializer());
+        serializerHashMap.put(dict.getByte(MessageTypeDictionary.MessageType.LOGOUTMSG), new LogoutMessageSerializer());
     }
     
     public void serialize(OutputStream out, byte type, Message m){
