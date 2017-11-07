d92254f9fae0436845afd9fbf5ff7ebba35e56f2
AlexKue <A.kuehnle@hotmail.de>
2017-11-08 00:19:48 +0100

    * added enums for new messages

--- src/chatroom/model/MessageTypeDictionary.java
+++ src/chatroom/model/MessageTypeDictionary.java
@@ -8,16 +8,18 @@ public class MessageTypeDictionary {
     private HashMap<MessageType,Byte> reverseHashMap;
 
     public enum MessageType {
-        SERVERMSG, PUBLICTEXTMSG, TARGETTEXTMSG, LOGINMSG, LOGOUTMSG
+        PUBLICSERVERMSG, PUBLICTEXTMSG, TARGETTEXTMSG, LOGINMSG, LOGOUTMSG, TARGETSERVERMSG, ACCEPTEDMSG
     }
 
     public MessageTypeDictionary() {
         messageTypeHashMap = new HashMap<>();
-        messageTypeHashMap.put((byte) 0, MessageType.SERVERMSG);
+        messageTypeHashMap.put((byte) 0, MessageType.PUBLICSERVERMSG);
         messageTypeHashMap.put((byte) 1, MessageType.PUBLICTEXTMSG);
         messageTypeHashMap.put((byte) 2, MessageType.TARGETTEXTMSG);
         messageTypeHashMap.put((byte) 3, MessageType.LOGINMSG);
         messageTypeHashMap.put((byte) 4, MessageType.LOGOUTMSG);
+        messageTypeHashMap.put((byte) 5, MessageType.TARGETSERVERMSG);
+        messageTypeHashMap.put((byte) 6, MessageType.ACCEPTEDMSG);
 
         for(byte key : messageTypeHashMap.keySet()){
             reverseHashMap.put(messageTypeHashMap.get(key), key);
