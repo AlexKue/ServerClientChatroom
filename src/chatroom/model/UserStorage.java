077624ab3ff96073e37838ca0346e4546e0fa5f4
AlexKue <A.kuehnle@hotmail.de>
2017-11-08 00:21:01 +0100

    * less ugly, hopefully more effective

--- src/chatroom/model/UserStorage.java
+++ src/chatroom/model/UserStorage.java
@@ -4,34 +4,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UserStorage {
-    HashMap<String, String[]> clients;
-    public void AddClient(String loginname, String password){
-        String[] data = {loginname, password};
-        clients.put(loginname, data);
+    ArrayList<UserAccountInfo> userInfo;
+    public UserStorage(){
+        userInfo = new ArrayList<>();
     }
 
-    public boolean checkExistingUsers(String loginname){
-        if (clients.get(loginname) == null) {
-            return false;
-        }
-        return true;
+    /**
+     * Registres a new User to the server, saving the loginname and the password
+     * @param loginname The loginname the User wants to register with
+     * @param password The password the User wishes to use
+     */
+    public void addClient(String loginname, String password){
+        userInfo.add(new UserAccountInfo(loginname, password));
     }
+
+    /**
+     * Checks if the user enters the correct loginname and the password correctly
+     * @param loginname The loginname the user wants to register with
+     * @param password The password the user wants to login with
+     * @return
+     */
     public boolean checkLogin(String loginname, String password){
-        if((clients.get(loginname))[1].equals(password)){
-            return true;
+        for (UserAccountInfo u : userInfo){
+            if(u.getLoginName().equals(loginname) && u.isPasswordCorrect(password)){
+                return true;
+            }
         }
-        else
-        {
-            return false;
-        }
-    }
-    public String getDisplayname(String loginname){
-        return (clients.get(loginname))[0];
-    }
-    public void setDisplayname(String loginname, String displayname){
-        String [] userdata = clients.get(loginname);    //Reading the data from the hashmap
-        userdata[0] = displayname;  //changin displayname
-        clients.put(loginname, userdata);   // adding the edited userdata with the new displayname to the hashmap
+        return false;
     }
 
+    /**
+     * Return the displayname of the logged in user
+     * @param loginname
+     * @return the name displayed for other clients
+     */
+    public String getDisplayname(String loginname){
+        for (UserAccountInfo u : userInfo){
+            if(u.getLoginName().equals(loginname)){
+                return u.getDisplayName();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Sets the name which should be visible for other clients
+     * @param loginname
+     * @param displayname
+     */
+    public void setDisplayname(String loginname, String displayname){
+        for (UserAccountInfo u : userInfo){
+            if(u.getLoginName().equals(u.getLoginName())){
+                u.setDisplayName(displayname);
+            }
+        }
+    }
 }
