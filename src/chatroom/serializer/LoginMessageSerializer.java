6b330d134776a52c258830393db91bdce862983f
AlexKue <A.kuehnle@hotmail.de>
2017-11-08 00:22:11 +0100

    * implemented serializer for loginmessage

--- src/chatroom/serializer/LoginMessageSerializer.java
+++ src/chatroom/serializer/LoginMessageSerializer.java
@@ -1,19 +1,31 @@
 package chatroom.serializer;
 
+import chatroom.model.LoginMessage;
 import chatroom.model.Message;
+import chatroom.model.MessageTypeDictionary;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import javax.xml.crypto.Data;
+import java.io.*;
 
 public class LoginMessageSerializer extends MessageSerializer {
     @Override
     public void serialize(OutputStream out, Message m) {
-
+        DataOutputStream dataOut = new DataOutputStream(new BufferedOutputStream(out));
+        try {
+            dataOut.writeByte(dict.getByte(MessageTypeDictionary.MessageType.LOGINMSG));
+            dataOut.writeUTF(((LoginMessage)m).getLoginName());
+            dataOut.writeUTF(((LoginMessage)m).getPassword());
+            dataOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Message deserialize(InputStream in) throws IOException {
-        return null;
+        DataInputStream dataIn = new DataInputStream(new BufferedInputStream(in));
+        String loginName = dataIn.readUTF();
+        String password = dataIn.readUTF();
+        return new LoginMessage(loginName,password);
     }
 }
