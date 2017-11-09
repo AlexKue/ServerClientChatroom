71e2b1a1663b135afe8c5ea8541082c25f8fc2e6
alexkue <alexander.kuehnle@uni-jena.de>
2017-11-09 01:57:50 +0100

    * Moving enum out of dictionary and made it public

--- src/chatroom/model/message/MessageTypeDictionary.java
+++ src/chatroom/model/message/MessageTypeDictionary.java
@@ -1,4 +1,4 @@
-package chatroom.model;
+package chatroom.model.message;
 
 import java.util.HashMap;
 
@@ -7,19 +7,17 @@ public class MessageTypeDictionary {
     private HashMap<Byte, MessageType> messageTypeHashMap;
     private HashMap<MessageType,Byte> reverseHashMap;
 
-    public enum MessageType {
-        PUBLICSERVERMSG, PUBLICTEXTMSG, TARGETTEXTMSG, LOGINMSG, LOGOUTMSG, TARGETSERVERMSG, ACCEPTEDMSG
-    }
-
     public MessageTypeDictionary() {
         messageTypeHashMap = new HashMap<>();
+        reverseHashMap = new HashMap<>();
+
         messageTypeHashMap.put((byte) 0, MessageType.PUBLICSERVERMSG);
         messageTypeHashMap.put((byte) 1, MessageType.PUBLICTEXTMSG);
         messageTypeHashMap.put((byte) 2, MessageType.TARGETTEXTMSG);
         messageTypeHashMap.put((byte) 3, MessageType.LOGINMSG);
         messageTypeHashMap.put((byte) 4, MessageType.LOGOUTMSG);
         messageTypeHashMap.put((byte) 5, MessageType.TARGETSERVERMSG);
-        messageTypeHashMap.put((byte) 6, MessageType.ACCEPTEDMSG);
+        messageTypeHashMap.put((byte) 6, MessageType.LOGINRESPONSEMSG);
 
         for(byte key : messageTypeHashMap.keySet()){
             reverseHashMap.put(messageTypeHashMap.get(key), key);
